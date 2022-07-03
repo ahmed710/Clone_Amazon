256698c38c28a6f4bde9bc3abd45d5d30b7bb1a6
boukhariahmed <87725419+boukhariahmed@users.noreply.github.com>
2022-07-03 12:16:33 +0100

    amazonT1-create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+function App() {
+  return (
+    <div>
+      <header >
+      <a href="/">AmazonTn</a>
+        
+      </header>
+      <main>
+        Products lists 
+      </main>
+    </div>
+  );
+}
+
+export default App;
